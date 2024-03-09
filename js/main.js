eb7440669cd5c60b82c1463b1bfd4b23e9931e8f
rifrifqyqy <142287042+rifrifqyqy@users.noreply.github.com>
2024-03-09 22:53:42 +0700

    main.js

--- js/main.js
+++ js/main.js
@@ -9,8 +9,28 @@ window.onload = function () {
 
   const menu_btn = document.querySelector(".hamburger");
   const mobile_menu = document.querySelector(".mobile-nav");
+  const scrollto = document.querySelector(".home-pg");
+  const scrolltoabout = document.querySelector(".about-pg");
+  const scrolltoservice = document.querySelector(".service-pg");
+  const scrolltofooter = document.querySelector(".contact-pg");
 
   menu_btn.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+  scrollto.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+  scrolltoabout.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+  scrolltoservice.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+  scrolltofooter.addEventListener("click", function () {
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
   });
